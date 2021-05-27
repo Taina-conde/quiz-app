77c7b5084d9e1490c791f1a2015a4ccc0e9bc1b2
Taina Conde <tainabatistaconde@gmail.com>
2021-05-27 18:08:01 -0400

    fix: Add name prop to FormControlLabel

--- src/components/QuizView/Question.js
+++ src/components/QuizView/Question.js
@@ -13,9 +13,8 @@ const Question = (props) => {
   const { question, id } = props;
   const [helperText, setHelperText] = useState("");
   let questionText = question.question;
-  const answers = question.incorrect_answers.concat(question.correct_answer)
+  const answers = question.incorrect_answers.concat(question.correct_answer);
   const correctAnswer = question.correct_answer;
-  
 
   const formik = useFormik({
     initialValues: {
@@ -27,36 +26,33 @@ const Question = (props) => {
       } else {
         setHelperText("Sorry, wrong answer!");
       }
-      alert(JSON.stringify(values, null, 2));
+      
     },
   });
 
   return (
     <Grid item>
       <form onSubmit={formik.handleSubmit}>
-        <FormControl
-          component="fieldset"
-          
-        >
+        <FormControl component="fieldset">
           <FormLabel component="legend">{questionText}</FormLabel>
-          <RadioGroup
-            aria-label="quiz"
-            name="quiz"
-            
-          >
-            {answers.map( (answer, index) => <FormControlLabel
-            key = {index}
-              value={answer}
-              control={<Radio />}
-              label={answer}
-            /> )}
+          <RadioGroup aria-label="quiz" name="quiz" value = {formik.values.picked} onChange = {formik.handleChange}>
+            {answers.map((answer, index) => (
+              <FormControlLabel
+                key={index}
+                name = "picked"
+                value = {answer}
+                control={<Radio />}
+                label={answer}
+                
+              />
+            ))}
           </RadioGroup>
           <FormHelperText>{helperText}</FormHelperText>
           <Button
             type="submit"
             variant="outlined"
             color="primary"
-            
+            disabled={formik.values.picked === ""}
           >
             Check Answer
           </Button>
