6a6da72fabcf36d9e0bd98525020bb6ad65abd9f
Taina Conde <tainabatistaconde@gmail.com>
2021-05-27 14:14:40 -0400

    feat: Wrap App component in ContextProvider in main index file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {ContextProvider} from './context';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ContextProvider>
     <App />
-  </React.StrictMode>,
+  </ContextProvider>,
   document.getElementById('root')
 );
 
