0464331ca7df5f08ecc49e806241567c0d321590
Taina Conde <tainabatistaconde@gmail.com>
2021-05-28 14:35:47 -0400

    feat: QuizView - add see results btn
    The button is enabled only if the number of questions answered is
    equal to the number of questions the user had to answer.

--- src/components/QuizView/index.js
+++ src/components/QuizView/index.js
@@ -1,11 +1,26 @@
 import { useContext } from "react";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import Context from "../../context";
 import QuizInitialButtons from "./QuizInitialButtons";
 import Question from "./Question";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  resultsBtn: {
+    alignSelf: "center",
+  },
+}));
 const QuizView = () => {
   const ctx = useContext(Context);
-  const { questions, numQuestions } = ctx;
+  const { questions, numQuestions, currentResults } = ctx;
+  console.log("questions", questions);
+  const questionsAnsweredArr = currentResults.questions
+    ? Object.keys(currentResults.questions)
+    : [];
+  const totalQuestionsAnswered = questionsAnsweredArr.length;
+
+  const classes = useStyles();
 
   if (numQuestions !== 0) {
     return (
@@ -18,14 +33,19 @@ const QuizView = () => {
   }
 
   return (
-    <Grid
-      container
-      direction="column"
-      spacing={5}
-    >
+    <Grid container direction="column" spacing={5}>
       {questions.map((question, index) => (
-        <Question key={index} question={question} id = {index + 1} />
+        <Question key={index} question={question} id={index + 1} />
       ))}
+      <Grid item className={classes.resultsBtn}>
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={questions.length !== totalQuestionsAnswered}
+        >
+          See results
+        </Button>
+      </Grid>
     </Grid>
   );
 };
