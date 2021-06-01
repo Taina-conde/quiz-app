afd78499ceab3a06cb90f17980f59aa006882d46
Taina Conde <tainabatistaconde@gmail.com>
2021-06-01 16:27:50 -0400

    style: ResultsView - Grid

--- src/components/ResultsView/AnswersList.js
+++ src/components/ResultsView/AnswersList.js
@@ -2,15 +2,14 @@ import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import AnswerItem from "./AnswerItem";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
-    marginTop : theme.spacing(4),
-   
   },
   item: {
-    justifyContent: "center"
+    justifyContent: "center",
   },
 }));
 
@@ -21,10 +20,13 @@ const AnswersList = (props) => {
   console.log("results na answersList", results);
   return (
     
-      <List component="section" aria-label="results" className = {classes.root}>
+      <List component="section" aria-label="results" className={classes.root}>
         {questionsArr.map((question, index) => (
-          <ListItem key = {index} className = {classes.item}> 
-              <AnswerItem question = {results.questions[question]} item = {index + 1}/>
+          <ListItem key={index} className={classes.item}>
+            <AnswerItem
+              question={results.questions[question]}
+              item={index + 1}
+            />
           </ListItem>
         ))}
       </List>
--- src/components/ResultsView/index.js
+++ src/components/ResultsView/index.js
@@ -1,16 +1,21 @@
 import Grid from "@material-ui/core/Grid";
 import { useParams } from "react-router-dom";
-import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import Score from "./Score";
 import AnswersList from "./AnswersList";
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  correctBox: {},
-  incorrectBox: {},
+  btn: {
+      display: "flex",
+      justifyContent: "center"
+  }
 }));
 
 const ResultsView = () => {
+    const classes = useStyles();
   const { resultsId } = useParams();
   const pastResults = JSON.parse(localStorage.getItem("pastResults"));
   console.log("past results", pastResults);
@@ -21,9 +26,16 @@ const ResultsView = () => {
         <Grid item>
           <Score resultsId={resultsId} results={pastResults[resultsId]} />
         </Grid>
-        <Grid item></Grid>
+        <Grid item>
+            <AnswersList resultsId = {resultsId} results = {pastResults[resultsId]}/>
+        </Grid>
+        <Grid item xs = "12" className = {classes.btn}>
+            <Button color = "primary" startIcon = {<ArrowBackIosIcon/>}>
+                Back to home
+            </Button>
+        </Grid>
       </Grid>
-      <AnswersList resultsId = {resultsId} results = {pastResults[resultsId]}/>
+      
     </>
   );
 };
