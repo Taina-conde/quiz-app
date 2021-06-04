4fb6ab56bf7b0d042506acd34f4d9f468103d972
Taina Conde <tainabatistaconde@gmail.com>
2021-06-04 12:47:48 -0400

    style: App - add Box around the container in App to add box shadow and light gray background

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,36 @@
-import logo from '../../logo.svg';
-import './App.css';
+import logo from "../../logo.svg";
+import "./App.css";
 import HomeView from "../HomeView";
-import  Container  from "@material-ui/core/Container";
-import QuizView from '../QuizView';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ResultsView from '../ResultsView';
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import QuizView from "../QuizView";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ResultsView from "../ResultsView";
+import {makeStyles} from "@material-ui/core/styles"
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: 0,
+
+  },
+  box: {
+    height: "100vh",
+    backgroundColor: "white",
+  }
+}))
 function App() {
+  const classes = useStyles();
   return (
-    <Router className="App">
+    <Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Container maxWidth = "md">
-        <Route path = "/" exact component= {HomeView}/>
-        <Route path = "/quiz" component = {QuizView} />
-        <Route path = "/results/:resultsId" component = {ResultsView}/>
+      <Container maxWidth="md" className = {classes.container}>
+        <Box boxShadow= {2} p={4} className = {classes.box} >
+          <Route path="/" exact component={HomeView} />
+          <Route path="/quiz" component={QuizView} />
+          <Route path="/results/:resultsId" component={ResultsView} />
+        </Box>
       </Container>
     </Router>
   );
